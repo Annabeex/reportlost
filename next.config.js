df141f608f501226e818f82365b7808e841560e4
Annabeex <lulu.denis44@gmail.com>
2025-12-21 12:55:55 +0100

    Redirect www to non-www

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
 
   async redirects() {
     return [
+      // ✅ Force www -> non-www (canonical)
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.reportlost.org",
+          },
+        ],
+        destination: "https://reportlost.org/:path*",
+        permanent: true, // 308
+      },
+
+      // ✅ Tes redirects existants
       {
         source: "/category/:slug",
         destination: "/lost-and-found/category/:slug",
@@ -28,4 +42,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
